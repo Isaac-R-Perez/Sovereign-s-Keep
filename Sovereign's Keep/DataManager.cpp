46efb507f7ac2c133be1d2ff85f63f1e4e3f0035
David Mendez <d.matthewm95@gmail.com>
2022-03-29 21:25:12 -0500

    Added some new classes and one enemy sime sprite in the images folder.

---
+++ Sovereign's Keep/DataManager.cpp
@@ -0,0 +1,11 @@
+#include "DataManager.h"
+
+void DataManager::SavePlayerData()
+{
+	// Save player stats to a PlayerData.game
+}
+
+void DataManager::LoadPlayerData()
+{
+	// Load player data from PlayerData.game
+}
---
+++ Sovereign's Keep/DataManager.h
@@ -0,0 +1,11 @@
+#pragma once
+class DataManager
+{
+/*
+ This class will save & load player data to & from a file.
+*/
+public:
+	static void SavePlayerData();
+	static void LoadPlayerData();
+};
+
