549dbd250ce3581c2ea610cdc8b94ce6ccb15eb4
David Mendez <d.matthewm95@gmail.com>
2022-04-14 11:21:59 -0500

    Added the skeleton for the LevelManger class. Still a WIP.

--- Sovereign's Keep/LevelManager.cpp
+++ Sovereign's Keep/LevelManager.cpp
@@ -1,1 +1,41 @@
 #include "LevelManager.h"
+
+LevelManager::LevelManager()
+{
+	Wave = 1;
+	WaveTimer = MAX_WAVE_TIME;
+	SpawnTickRate = 2.0f; //Spawn an enemy every 2 seconds
+}
+
+void LevelManager::update(double dt)
+{
+	if (WaveTimer > 0)
+	{
+		WaveTimer -= dt;
+	}
+	else
+	{
+		GenerateNextWave();
+		WaveTimer = MAX_WAVE_TIME;
+		Wave++;
+	}
+
+	//TODO - Spawn the enemies here based on tick rate
+}
+
+void LevelManager::GenerateNextWave()
+{
+	//TODO - Change the vector of enemies to spawn for the wave.
+	//OPTIONAL - You could also adjust the SpawnTickRate for the new wave.
+}
+
+void LevelManager::SpawnEnemy()
+{
+	// Spawns only one enemy randomly picked from the Wave Vector
+}
+
+void LevelManager::SpawnEnemyGroup()
+{
+	// Should select a group of enemies (min 3) from the Wave vector
+	// and spawn them together.
+}
--- Sovereign's Keep/LevelManager.h
+++ Sovereign's Keep/LevelManager.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <vector>
+#include "Game.h"
+#include "Enemy.h"
+
+const float MAX_WAVE_TIME = 60.0f;
+
 class LevelManager
 {
+private:
+	int Wave; //The current wave the level is on.
+	float WaveTimer; //How long the current wave lasts
+	float SpawnTickRate; //What interval to spawn enemies in the current wave.
 
+	//TODO - Have a vector filled with enemies to spawn for the current wave.
+
+public:
+	LevelManager();
+	void update(double dt);
+	void GenerateNextWave();
+
+	void SpawnEnemy();
+	void SpawnEnemyGroup();
 };
